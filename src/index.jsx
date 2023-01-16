978c158a0b33c5b0377368ccc5e9aeb435b3edf5
lautaroturic <lautaroturic@gmail.com>
2023-01-16 04:48:48 -0300

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
-import getFirebase, {analitycs} from './firebase/config';
-analitycs();
+import getFirebase, {analytics} from './firebase/config';
+analytics();
 getFirebase();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
