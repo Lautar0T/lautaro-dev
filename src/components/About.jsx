d97a7a7f76dcdada17623a4284076c6a3cf981b8
lautaroturic <lautaroturic@gmail.com>
2023-01-17 00:07:00 -0300

    dale

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     <section className=" relative p-5  w-screen min-h-full">
       <div className="relative top-6 left-7  w-max p-2">
         <p className=" text-2xl">Lautaro</p>
-        <p>Software Engineer and Web Developer</p>
+        <p>Software Engineer and Web Developer daleeeeeee</p>
       </div>
     </section>
   )
